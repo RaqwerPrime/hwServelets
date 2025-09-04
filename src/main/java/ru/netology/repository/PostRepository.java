cf6c2c70bbb1067de2c743c525fd282e0485dc2c
Ruslan Araslanov <allmagrave@gmail.com>
2025-09-04 14:34:04 +0300

    Final Result

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,42 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class PostRepository {
+    private final Map<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong currentId = new AtomicLong(1);
+
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            long newId = currentId.getAndIncrement();
+            Post newPost = new Post(newId, post.getContent());
+            posts.put(newId, newPost);
+            return newPost;
+        } else {
+            if (posts.containsKey(post.getId())) {
+                posts.put(post.getId(), post);
+                return post;
+            } else {
+                posts.put(post.getId(), post);
+                currentId.updateAndGet(current -> Math.max(current, post.getId() + 1));
+                return post;
+            }
+        }
+    }
+
+    public void removeById(long id) {
+        posts.remove(id);
+    }
+}
